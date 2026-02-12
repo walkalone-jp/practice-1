89fae498cf1713e4cb5dc36e677862732dda4042
walkalone-jp <walkalone.jp@gmail.com>
2026-02-12 14:58:27 +0900

    chore: .firebaserc and add Firebase deploy workflow

--- spabase.ts
+++ spabase.ts
@@ -1,10 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
-// Create a single supabase client for interacting with your database
+// データベースと相互作用するための単一のsupabaseクライアントを作成
 
 const supabase = createClient(
 
-process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
 
-process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
 
 )
